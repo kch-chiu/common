1b0634a8a2576fcc02057a8001c0814e3cfa7cdf
kchiu-dev <70428366+kchiu-dev@users.noreply.github.com>
2020-09-04 23:10:41 -0400

    update project configuration

---
+++ index.ts
@@ -0,0 +1,23 @@
+// Re-export stuff from errors and middlewares
+export * from "./src/errors/bad-request-error";
+export * from "./src/errors/custom-error";
+export * from "./src/errors/database-connection-error";
+export * from "./src/errors/not-authorized-error";
+export * from "./src/errors/not-found-error";
+export * from "./src/errors/request-validation-error";
+
+export * from "./src/middlewares/current-user";
+export * from "./src/middlewares/error-handler";
+export * from "./src/middlewares/require-auth";
+export * from "./src/middlewares/validate-request";
+
+export * from "./src/events/base-listener";
+export * from "./src/events/base-publisher";
+export * from "./src/events/subjects";
+export * from "./src/events/ticket-created-event";
+export * from "./src/events/ticket-updated-event";
+export * from "./src/events/types/order-status";
+export * from "./src/events/order-cancelled-event";
+export * from "./src/events/order-created-event";
+export * from "./src/events/expiration-complete-event";
+export * from "./src/events/payment-created-event";
