fe5e861c12fad91e68d8a2adeec5a42ac4067457
kchiu-dev <70428366+kchiu-dev@users.noreply.github.com>
2020-09-07 21:52:03 -0400

    Update custom-error.ts

--- src/errors/custom-error.ts
+++ src/errors/custom-error.ts
@@ -4,8 +4,6 @@ export abstract class CustomError extends Error {
   constructor(message: string) {
     super(message);
 
-    console.log("ERROR!");
-
     Object.setPrototypeOf(this, CustomError.prototype);
   }
 
