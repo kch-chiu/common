dff9853d7a0307fd71d8f909947e5f7def1d130f
kch-chiu <gh-actions-kch-chiu@github.com>
2021-02-25 12:10:48 -0500

    Update payment-created-event.ts

--- src/events/payment-created-event.ts
+++ src/events/payment-created-event.ts
@@ -3,8 +3,6 @@ import { Subjects } from "./subjects";
 export interface PaymentCreatedEvent {
   subject: Subjects.PaymentCreated;
   data: {
-    id: string;
-    orderId: string;
     stripeId: string;
   };
 }
